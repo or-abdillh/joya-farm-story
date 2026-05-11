38e21fd2df487852f37492475b86834066cf0981
or-abdillh <hans.abdillh05@gmail.com>
2026-05-11 18:00:41 +0800

    chore: prep deps and theme base

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    qualities: [75, 85, 95],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
